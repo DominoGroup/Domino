b3063ea784223396b7560cae993b7838eb46364c
Blastom Nuke <blastom1016@gmail.com>
2017-07-24 23:14:12 +0800

    修改计划 - Excel仅在编辑器使用，发布序列化会使用二进制

--- Client/Assets/Code/Manager_Data/MapItemData.cs
+++ Client/Assets/Code/Manager_Data/MapItemData.cs
@@ -1,7 +1,14 @@
-﻿/// <summary>
+﻿using UnityEngine;
+/// <summary>
 /// 地图物品数据
 /// </summary>
-public class MapItemData
+[System.Serializable]
+public class MapItemData : ExcelData
 {
     public int id;
+    public string name;
+    public Vector3 size;
+    public float mass;
+    public float drag;
+    public float angularDrag;
 }
