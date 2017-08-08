9de249fc80bbdd2838881c59049f062c14e5b171
hyemptygod <909325341@qq.com>
2017-08-08 23:01:25 +0800

    Excel文件读取
    Excel文件读取

--- Client/Assets/Code/Editor/Excel/ExcelDataEditor.cs
+++ Client/Assets/Code/Editor/Excel/ExcelDataEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using System.IO;
 using System.Reflection;
 using Excel;
@@ -40,18 +41,72 @@ public class ExcelDataEditor
                         else
                         {
                             // 赋值根据Field类型予以赋值的方法需要增加
-                            throw new System.NotImplementedException();
-                            //fields[i].SetValue(item, );
+                            //throw new System.NotImplementedException();
+                            field.SetValue(item, ConvertObject(row[j], field.FieldType));
                         }
                     }
+                    result.Add(item);
                 }
 
             }
         }
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
+        return result;
     }
     static string GetExcelFilePath(string excelName)
     {
         return Application.dataPath + Path.DirectorySeparatorChar + excelFileRoot + Path.DirectorySeparatorChar + excelName + excelFileExtension;
+    }
+
+    /// <summary>
+    /// 类型转换
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    static public object ConvertObject(object obj, System.Type type)
+    {
+        if (type == null) return obj;
+
+        if (obj == null) return type.IsValueType ? Activator.CreateInstance(type) : null;
+
+        Type underlyingType = Nullable.GetUnderlyingType(type);
+        if (type.IsAssignableFrom(obj.GetType())) // 如果待转换对象的类型与目标类型兼容，则无需转换
+        {
+            return obj;
+        }
+        else if ((underlyingType ?? type).IsEnum) // 如果待转换的对象的基类型为枚举
+        {
+            if (underlyingType != null && string.IsNullOrEmpty(obj.ToString())) // 如果目标类型为可空枚举，并且待转换对象为null 则直接返回null值
+            {
+                return null;
+            }
+            else
+            {
+                return Enum.Parse(underlyingType ?? type, obj.ToString());
+            }
+        }
+        else if (typeof(IConvertible).IsAssignableFrom(underlyingType ?? type)) // 如果目标类型的基类型实现了IConvertible，则直接转换
+        {
+            try
+            {
+                return Convert.ChangeType(obj, underlyingType ?? type, null);
+            }
+            catch
+            {
+                return underlyingType == null ? Activator.CreateInstance(type) : null;
+            }
+        }
+        else
+        {
+            var splitValue = (obj as string).Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            var args = new object[splitValue.Length];
+            
+            for (int i = 0; i < splitValue.Length; i++)
+            {
+                args[i] = ConvertObject(splitValue[i], typeof(float));
+            }
+            return Activator.CreateInstance(type, args);
+        }
     }
 }
